12e8430cb3a014722638220dd84284d19d82c54b
Thúy Hảo <leminhlongit@gmail.com>
2024-06-21 17:43:59 +0700

    up

--- build/generated/sources/annotationProcessor/java/main/site/sugarnest/backend/mapper/IAccountMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/site/sugarnest/backend/mapper/IAccountMapperImpl.java
@@ -20,7 +20,7 @@ import site.sugarnest.backend.entities.RoleEntity;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-06-21T17:26:15+0700",
+    date = "2024-06-21T17:42:05+0700",
     comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.7.jar, environment: Java 18.0.2 (Amazon.com Inc.)"
 )
 @Component
